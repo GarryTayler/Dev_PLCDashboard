cb9bf142bab75a7066381932e0a92cb4d86fe359
DESKTOP-8R8JHMP\garry <blackstar0811@outlook.com>
2021-03-30 14:41:23 +0900

    [logic_setting] 로직설정/ 변수목록 페이지 pagination 작동 & 현재 page 위치 저장 & 변수 팝업 모달 pagination수정

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -302,6 +302,19 @@ function setDataTblPage(tblInst, dispLen) {
     }
 }
 
+function setDataVariableTblPage(tblInst, dispLen) {
+
+    tblInst.attr('before_clicked', '0');
+    localStorage.setItem('page-cnt', dispLen);
+
+    const curPage = localStorage.getItem('variable-cur-page');
+    if(dataTables && curPage && curPage > 0) {
+        localStorage.removeItem('variable-cur-page');
+        dataTables.page(parseInt(curPage));
+        updateVariable();
+    }
+}
+
 function initTimeMask(parentDiv = '') {
     const maskArr = $(parentDiv + " .mask-time");
     for(let ii = 0; ii < maskArr.length; ii++) {
